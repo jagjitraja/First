33e8203c6d687633067b786afde7a46cb6099d6a
Jagjit Singh <jsingh@atimi.com>
2016-12-28 16:57:35 -0800

    Added Category choice dialogue

--- Android/NoteTaker/app/src/main/java/jsb/com/notetaker/NoteEditFragment.java
+++ Android/NoteTaker/app/src/main/java/jsb/com/notetaker/NoteEditFragment.java
@@ -1,9 +1,11 @@
 package jsb.com.notetaker;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,9 @@ public class NoteEditFragment extends Fragment {
 	private ImageButton imageButton;
 	private EditText editTitle;
 	private EditText editBody;
+	private AlertDialog.Builder chooseCategoryDialogue;
+
+    private Note.Category chosenCategory;
 
 	public NoteEditFragment() {
 		// Required empty public constructor
@@ -30,6 +35,8 @@ public class NoteEditFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 
+
+
 		View fragmentLayout = inflater.inflate(R.layout.fragment_note_edit,container,false);
 
 		imageButton = (ImageButton) fragmentLayout.findViewById(R.id.edit_note_item_icon);
@@ -38,16 +45,74 @@ public class NoteEditFragment extends Fragment {
 
 		Intent intent = getActivity().getIntent();
 
+
 		String title = intent.getExtras().getString(MainActivity.NOTE_TITLE_KEY);
 		String body = intent.getExtras().getString(MainActivity.NOTE_BODY_KEY);
-		Note.Category category = (Note.Category) intent.getExtras().getSerializable(MainActivity.NOTE_CATEGORY_KEY);
-
+        Note.Category category;
+        if(savedInstanceState ==null) {
+             category = (Note.Category) intent.getExtras().getSerializable(MainActivity.NOTE_CATEGORY_KEY);
+            chosenCategory = category;
+        }
+        else{
+            category = (Note.Category) savedInstanceState.getSerializable(MainActivity.NOTE_CATEGORY_KEY);
+            chosenCategory = category;
+        }
 		imageButton.setImageResource(Note.getCategoryImageFromCategory(category));
-		editTitle.setText(title);
+
+
+            editTitle.setText(title);
 		editBody.setText(body);
 
-		return inflater.inflate(R.layout.fragment_note_edit, container, false);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchChooseCategoryDialogueBuilder();
+                chooseCategoryDialogue.show();
+            }
+        });
+
+		return fragmentLayout;
 	}
+
+
+	private void launchChooseCategoryDialogueBuilder(){
+
+		chooseCategoryDialogue = new AlertDialog.Builder(getContext());
+
+        final String[] categories = {"PRIVATE","MEALS","WORK","FINANCIAL","STUDIES"};
+
+        chooseCategoryDialogue.setTitle(getResources().getString(R.string.choose_category_dialogue_title));
+
+        chooseCategoryDialogue.setSingleChoiceItems(categories, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                chosenCategory = Note.getCategoryFromString(categories[which]);
+                imageButton.setImageResource(Note.getCategoryImageFromCategory(chosenCategory));
+                dialog.dismiss();
+            }
+        });
+
+        chooseCategoryDialogue.setNegativeButton(getResources().getString(R.string.cancel_button),
+                new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        chooseCategoryDialogue.create();
+
+	}
+
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState){
+
+        saveInstanceState.putSerializable(MainActivity.NOTE_CATEGORY_KEY,chosenCategory);
+
+    }
 
 	@Override
 	public void onDestroy() {
